6413949865b2a2645543138f6b8a4e678a8faad9
Arif <kanebrian0634@gmail.com>
2022-06-27 22:27:56 +0300

    hooks added

--- rc05_events-useStateHook/src/App.js
+++ rc05_events-useStateHook/src/App.js
@@ -1,11 +1,13 @@
 import Events from "./components/1-event-intro/Events";
 import Counter from "./components/2-classComponent/Counter";
+import UseStateExample from "./components/3-useStateHook/UseStateExample";
 
 function App() {
   return (
     <div>
       <Events />
       <Counter />
+      <UseStateExample />
 
     </div>
   );
