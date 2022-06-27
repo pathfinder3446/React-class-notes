ee85a96c79620ac22d1653c4683099bc330d0478
Arif <kanebrian0634@gmail.com>
2022-06-27 23:06:37 +0300

    ..

--- rc05_events-useStateHook/src/components/3-useStateHook/UseStateExample.jsx
+++ rc05_events-useStateHook/src/components/3-useStateHook/UseStateExample.jsx
@@ -20,9 +20,58 @@
 
 import { useState } from "react";
 
-
 const UseStateExample = () => {
-    return <div>UseStateExample</div>;
+  const [count, setCount] = useState(0);
+
+  const [info, setInfo] = useState({
+    name: "Ahmet Yılmaz",
+    email: "ay@gmail.com",
+    age: 25,
+  });
+
+  const inc = () => {
+    setCount(count + 1);
   };
-  
-  export default UseStateExample;
+
+  const dec = () => {
+    setCount(count - 1);
+  };
+
+  const clr = () => {
+    setCount(0);
+  };
+
+  const incAge = () => {
+    setInfo({...info, age:info.age+1});
+    // setInfo({name:"ALİ VELİ", email:"hgy@hotmail", age:55});
+
+  }
+
+  return (
+    <div className="container text-center mt-4">
+      <section>
+        <h1 className="display-4 text-danger m-4">USESTATE COUNT:{count}</h1>
+        <button onClick={inc} className="btn btn-success">
+          INC
+        </button>
+        {/* <button onClick={() => setCount(0)} className="btn btn-success">INC</button> //* alternatif yöntem fonksiyonu ilgili elementte de tanımlayabilirdim. */}
+        <button onClick={clr} className="btn btn-dark">
+          CLR
+        </button>
+        <button onClick={dec} className="btn btn-warning">
+          DEC
+        </button>
+      </section>
+
+      <section>
+        <h1 className="display-4 text-danger m-4">USESTATE OBJECT</h1>
+        <h2>{info.name}</h2>
+        <h3>{info.email}</h3>
+        <h3>{info.age}</h3>
+        <button onClick={incAge} className="btn btn-info">inc age</button>
+      </section>
+    </div>
+  );
+};
+
+export default UseStateExample;
