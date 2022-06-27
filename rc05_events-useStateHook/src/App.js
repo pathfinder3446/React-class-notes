368afd35ab7a061a3e84ca6b0fa41f924a40376a
Arif <kanebrian0634@gmail.com>
2022-06-27 22:06:54 +0300

    ..

--- rc05_events-useStateHook/src/App.js
+++ rc05_events-useStateHook/src/App.js
@@ -1,9 +1,11 @@
 import Events from "./components/1-event-intro/Events";
+import Counter from "./components/2-classComponent/Counter";
 
 function App() {
   return (
     <div>
       <Events />
+      <Counter />
 
     </div>
   );
