37bff1e20e50e50ab0b29f251c687f0b032d182e
Arif <kanebrian0634@gmail.com>
2022-06-28 23:02:13 +0300

    ..

--- rc06_forms_otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
+++ rc06_forms_otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,7 +1,27 @@
-
+import {FaReact} from 'react-icons/fa';
+import {FaEvernote} from 'react-icons/fa';
 
 const MouseEvent = () => {
-  return <div>MouseEvent</div>;
+  return  <div className="container text-center d-flex flex-column align-items-center mt-4">
+  <h2 className="text-danger">MOUSE EVENTS</h2>
+
+  <div id="todo-1" className="bg-success text-light w-50 p-4">
+    todo item 1 <FaReact />
+  </div>
+
+  <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
+    todo item 2 <FaEvernote />
+  </div>
+
+  <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
+    todo item 3 <FaReact />
+  </div>
+
+  <p>X and Y</p>
+  {/* <p className="text-danger fw-bold">
+    {coordX} {coordY}
+  </p> */}
+</div>;
 };
 
 export default MouseEvent;
