0ed020ea638120a69fd1a526a34265e5ed215c48
Arif <kanebrian0634@gmail.com>
2022-06-30 21:48:56 +0300

    ..

--- rc08_lifecycle-useEffect/src/App.js
+++ rc08_lifecycle-useEffect/src/App.js
@@ -1,9 +1,20 @@
 import LifeCycleMethods from './components/LifeCycleMethods';
+import { useState } from 'react';
+import UseEffectHook from './components/UseEffectHook'
 
 function App() {
+
+  const [show, setShow] = useState(true);
+  console.log(show)
+
+
   return (
     <div className="container text-center mt-4">
-      <LifeCycleMethods />
+      <button className='btn btn-dark' onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+      {/* {show && <LifeCycleMethods /> } */}
+      {show && <UseEffectHook /> }
+      
+      
     </div>
   );
 }
