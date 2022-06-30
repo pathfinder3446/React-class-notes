3f8cf6c95211ec2750a483e24d5eb0737065c4ec
Arif <kanebrian0634@gmail.com>
2022-06-30 22:34:16 +0300

    ..

--- rc08_lifecycle-useEffect/src/components/UseEffectHook.jsx
+++ rc08_lifecycle-useEffect/src/components/UseEffectHook.jsx
@@ -34,13 +34,38 @@ import { useState, useEffect } from "react";
 const UseEffectHook = () => {
     const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        console.log("Mounted");
-        setTimeout(() => {
-            alert("Data fetched")
-        }, 3000)
-    }, [])
+
+    //! ComponentDidMount
+    // useEffect(() => {
+    //     console.log("Mounted");
+    //     setTimeout(() => {
+    //         alert("Data fetched")
+    //     }, 3000)
+    // }, [])
+
+    //! ComponentDidMount + ComponentDidUpdate
+    // useEffect(() => {
+    //     console.log("Mounted + Updated");
+    //     setTimeout(() => {
+    //         alert("Data fetched")
+    //     }, 1000)
+    // })
   
+      //! ComponentDidMount + componentWillUnmount
+  const fetchData = () => {
+    console.log('Data Fetched');
+  };
+
+  useEffect(() => {
+    const timerId = setInterval(fetchData, 1000);
+    console.log('Mounted');
+    return () => {
+      clearInterval(timerId);
+      console.log('Unmounted');
+    };
+  }, []);
+
+
     console.log("rendered");
     return (
       <div className="container text-center mt-4">
