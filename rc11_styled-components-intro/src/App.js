2ba8d4a96c196058e1c46c7820dae9da0ce35677
Arif <kanebrian0634@gmail.com>
2022-07-05 21:12:14 +0300

    ..

--- rc11_styled-components-intro/src/App.js
+++ rc11_styled-components-intro/src/App.js
@@ -1,6 +1,25 @@
+import Button, { TomatoButton } from './components/Button.styled';
+import Container from './components/Container.styled.';
+import HeaderText from './components/Header.styled';
+import StyledLink from './components/Link.styled';
 
 const App = () => {
-  return <h1>Styled</h1>;
+  return (
+    <>
+    <Container bg="green">
+      <HeaderText color='black'>Styled Components</HeaderText>
+      <Button primary>Click</Button>
+      <Button>Click</Button>
+      <TomatoButton>Click</TomatoButton>
+      <TomatoButton primary>Click</TomatoButton>
+      <HeaderText>Styled Components</HeaderText>
+      <StyledLink href="https://clarusway.com/" target="_blank">
+          Clarusway
+        </StyledLink>
+    </Container>
+      
+    </>
+  );
 };
 
 export default App;
