fd7617838d54f2c36479481df2403d860fd8591a
Arif <kanebrian0634@gmail.com>
2022-07-16 13:48:05 +0300

    ..

---
+++ rc16_context-reducer/reducer-example/src/reducer.jsx
@@ -0,0 +1,18 @@
+export const initialState ={
+    loading: false,
+    dog: '',
+    error: '',
+};
+
+export const reducer = (state, action) =>{
+
+    switch (action.type)) {
+        case 'START':
+            
+            break;
+    
+        default:
+            break;
+    }
+
+}
