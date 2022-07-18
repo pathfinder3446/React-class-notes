e1ebb3399b1b458ecd1ea85a8ca479555c944673
Arif <kanebrian0634@gmail.com>
2022-07-18 23:16:24 +0300

    ..

--- rc17_material-ui/material-intro/src/pages/Home.jsx
+++ rc17_material-ui/material-intro/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import TextFieldComp from '../components/TextFieldComponent';
 import TypoButtons from '../components/TypoButtons';
 import CardsGrid from '../components/CardsGrid';
+import NavBar from '../components/AppBarComponent';
 
 const Home = () => {
   return (
     <>
+      <NavBar />
       {/* <h1>HOME</h1> */}
       {/* <TypoButtons /> */}
       {/* <TextFieldComp/> */}
