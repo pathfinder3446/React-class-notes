7ee227ae2d7154524d352c636f4c10c4e30ee930
Arif <kanebrian0634@gmail.com>
2022-07-21 18:59:25 +0300

    ..

--- rc18-19_movie-app-solution/src/auth/firebase.js
+++ rc18-19_movie-app-solution/src/auth/firebase.js
@@ -6,12 +6,12 @@ import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 //* https://console.firebase.google.com/ => project settings
 //! firebase console settings bölümünden firebaseconfig ayarlarını al
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
