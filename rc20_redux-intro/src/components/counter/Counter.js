cb7fc6d0706024eea3aff64dfcc54db6b73d7393
Arif <kanebrian0634@gmail.com>
2022-07-22 12:24:52 +0300

    ..

--- rc20_redux-intro/src/components/counter/Counter.js
+++ rc20_redux-intro/src/components/counter/Counter.js
@@ -1,6 +1,7 @@
 
 import './Counter.css'
 import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from '../../redux/actions/counterActions';
 
 const Counter = () => {
 
@@ -12,9 +13,18 @@ const Counter = () => {
       <h2 className="counter-header">Counter With Redux</h2>
       <h1>{counter}</h1>
       <div>
-        <button onClick={() => dispatch({type:'INCREMENT'})} className="counter-button positive">increase</button>
-        <button onClick={() => dispatch({type:'RESET'})} className="counter-button zero">reset</button>
-        <button onClick={() => dispatch({type:'DECREMENT'})} className="counter-button negative">decrease</button>
+        <button 
+        // onClick={() => dispatch({type:'INCREMENT'})} 
+        onClick={() => dispatch(increment())} 
+        className="counter-button positive">increase</button>
+        <button 
+        // onClick={() => dispatch({type:'RESET'})} 
+        onClick={() => dispatch(reset())} 
+        className="counter-button zero">reset</button>
+        <button 
+        // onClick={() => dispatch({type:'DECREMENT'})} 
+        onClick={() => dispatch(decrement())} 
+        className="counter-button negative">decrease</button>
       </div>
     </div>
   );
