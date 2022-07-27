c8f9ca47f9152ec83e83e6b765fe500e8918d5c5
Arif <kanebrian0634@gmail.com>
2022-07-27 12:30:30 +0300

    ..

--- rc23_useref-usememo-usecallback/src/components/Header.jsx
+++ rc23_useref-usememo-usecallback/src/components/Header.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { memo } from 'react';
+import cw from '../assets/cw_logo.png';
 
-const Header = () => {
+//* React.memo componentleri hafızaya almak için kullanılır, render'ı önler
+
+const Header = ({ img }) => {
+  console.log('Rendering: Header Comp.');
   return (
-    <div>
-      <h2>Header</h2>
+    <div className="header">
+      <img
+        src={img ? img : cw}
+        alt="cw_logo"
+        style={{ margin: '1rem', maxHeight: '200px' }}
+      />
     </div>
   );
 };
 
-export default Header;
+export default memo(Header);
+
+//* 2.kullanım
+// const Header = memo(({ img }) => {
+
+//   console.log('Rendering: Header Comp.');
+//   return (
+//     <div className="header">
+//       <img
+//         src={img ? img : cw}
+//         alt="cw_logo"
+//         style={{ margin: '1rem', maxHeight: '200px' }}
+//       />
+//     </div>
+//   );
+// });
+
+// export default Header;
+
+//* 1.kullanım
+// const Header = React.memo(({ img }) => {
+
+//   console.log('Rendering: Header Comp.');
+//   return (
+//     <div className="header">
+//       <img
+//         src={img ? img : cw}
+//         alt="cw_logo"
+//         style={{ margin: '1rem', maxHeight: '200px' }}
+//       />
+//     </div>
+//   );
+// });
+
+// export default Header;
